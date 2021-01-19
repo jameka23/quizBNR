ba9d038ef201ba840f30ac3c007d701d5733240b
Jameka Echols <jameka.echols@gmail.com>
2021-01-18 20:34:08 -0600

    added buttons and the labels

---
+++ Quiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by Jameka Echols on 1/18/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
